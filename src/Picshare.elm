9088e1bba892bb0d10709bedaca200a643c0d2da
Ryan B <ryan.brilliant.nirwana@gmail.com>
2023-12-08 03:49:46 +0700

    handle state changes - page 46 add heart button

--- src/Picshare.elm
+++ src/Picshare.elm
@@ -1,7 +1,9 @@
 module Picshare exposing (main)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
 
 
 baseUrl : String
@@ -9,23 +11,48 @@ baseUrl =
     "https://programming-elm.com/"
 
 
-initialModel : { url : String, caption : String }
+initialModel : { url : String, caption : String, liked : Bool }
 initialModel =
     { url = baseUrl ++ "4.jpg"
     , caption = "Wood"
+    , liked = False
     }
 
 
-viewDetailedPhoto : { url : String, caption : String } -> Html msg
+viewDetailedPhoto : { url : String, caption : String, liked : Bool } -> Html Msg
 viewDetailedPhoto model =
+    let
+        buttonClass =
+            if model.liked then
+                "fa-heart"
+
+            else
+                "fa-heart-o"
+
+        msg =
+            if model.liked then
+                Unlike
+
+            else
+                Like
+    in
     div [ class "detailed-photo" ]
         [ img [ src model.url ] []
         , div [ class "photo-info" ]
-            [ h2 [ class "caption" ] [ text model.caption ] ]
+            [ div [ class "like-button" ]
+                [ i
+                    [ class "fa fa-2x"
+                    , class buttonClass
+                    , onClick msg
+                    ]
+                    []
+                ]
+            , h2 [ class "caption" ] [ text model.caption ]
+            ]
         ]
 
 
-view : { url : String, caption : String } -> Html msg
+view : { url : String, caption : String, liked : Bool } -> Html Msg
 view model =
     div []
         [ div [ class "header" ]
@@ -35,6 +62,28 @@ view model =
         ]
 
 
-main : Html msg
+type Msg
+    = Like
+    | Unlike
+
+
+update :
+    Msg
+    -> { url : String, caption : String, liked : Bool }
+    -> { url : String, caption : String, liked : Bool }
+update msg model =
+    case msg of
+        Like ->
+            { model | liked = True }
+
+        Unlike ->
+            { model | liked = False }
+
+
+main : Program () { url : String, caption : String, liked : Bool } Msg
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
